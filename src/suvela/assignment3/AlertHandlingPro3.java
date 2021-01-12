d338d052a141dafaa18df1a2cf334f49f5e27e05
Suvela khaladkar <suvela.vela16@gmail.com>
2021-01-12 22:32:55 +0530

    Suvela_assign3

---
+++ src/suvela/assignment3/AlertHandlingPro3.java
@@ -0,0 +1,50 @@
+package suvela.assignment3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertHandlingPro3 {
+	static WebDriver driver;
+
+	public void start() {
+		String path = "./resources/windows/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		driver = new ChromeDriver();
+		driver.get("http://automationbykrishna.com");
+		driver.manage().window().maximize();
+	}
+	public void verifyForm() throws Exception {
+		System.out.println("click on basic elements tab");
+		driver.findElement(By.xpath("//a[@id='basicelements']")).click();
+		Thread.sleep(1000);
+		
+		System.out.println("Enter first name");
+		String fname="Suvela";
+		driver.findElement(By.xpath("//input[@name='ufname']")).sendKeys(fname);
+		System.out.println("Enter last name");
+		String lname="Khaladkar";
+		driver.findElement(By.xpath("//input[@name='ulname']")).sendKeys(lname);
+		System.out.println("Enter company name");
+		String company="ABC";
+		driver.findElement(By.xpath("//input[@name='cmpname']")).sendKeys(company);
+		
+		System.out.println("Hit submit");
+		driver.findElement(By.xpath("//div[@id='firstRow']/div[1]//button[@type='submit']")).click();
+		
+		String actualmsg=fname +" and " +lname+ " and "+company;
+		String alertmsg=driver.switchTo().alert().getText();
+		if(alertmsg.equals(actualmsg))
+			System.out.println("Alert has been handled");
+		else
+			System.out.println("Alert has been cancled");
+		driver.switchTo().alert().dismiss();
+		driver.close();
+	}
+	public static void main(String[] args) throws Exception {
+		new AlertHandlingPro3().start();
+		new AlertHandlingPro3().verifyForm();
+
+	}
+
+}
