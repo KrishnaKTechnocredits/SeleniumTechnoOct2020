cdb5b67a89de804d0f629749da9c150751ea2437
Ankita Dahiwelkar <ankitadahiwelkar52@gmail.com>
2021-01-14 22:51:05 +0530

    Ankita_Asgnmnt_5

---
+++ src/ankita/base/PredefinedActions.java
@@ -0,0 +1,17 @@
+package ankita.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class PredefinedActions {
+	public static WebDriver start() {
+		return start("http://automationbykrishna.com");
+	}
+
+	public static WebDriver start(String url) {
+		System.setProperty(ConstantPath.chromeDriverKey, ConstantPath.chromeDriverPath);
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		return driver;
+	}
+}
