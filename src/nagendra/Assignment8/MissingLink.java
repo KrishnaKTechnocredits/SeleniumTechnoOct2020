836a2629fce0e7c94aaeec55fdff3c812c43c4d8
Nagendra Tiwari <tiwarinagendra11@gmail.com>
2021-01-15 17:55:23 +0530

    NagendraT_Assignment8

---
+++ src/nagendra/Assignment8/MissingLink.java
@@ -0,0 +1,43 @@
+package nagendra.Assignment8;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import technoCredits.base.PredefinedActions;
+
+/*
+1. Print missing link button text
+Note: use missing link form from resources folder
+*/
+
+public class MissingLink {
+
+	public static void printMissingLinkButtonText(WebDriver driver) {
+
+		System.out.println("STEP - Provided Implicit Wait to Driver Object");
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+		System.out.println("STEP - Find All The Links on a WebPage");
+		List<WebElement> links = driver.findElements(By.xpath("//a"));
+		for (WebElement element : links) {
+			try {
+				if (element.getAttribute("href").equals("")) {
+					System.out.println("Missing link is - " + element.getText());
+				}
+			} catch (NullPointerException npe) {
+				System.out.println("Missing link is without href tag - " + element.getText());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		WebDriver driver = PredefinedActions.start(
+				"file:///C:/Users/Nagendra/Documents/Training/Training/SeleniumTechnoOct2020/resources/forms/MissingLink.html");
+		printMissingLinkButtonText(driver);
+		System.out.println("STEP - Close the driver");
+		driver.close();
+		System.out.println("STEP - Test Completed");
+
+	}
+}
