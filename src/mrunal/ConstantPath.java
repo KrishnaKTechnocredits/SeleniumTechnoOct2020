198d746dda9791897613270a2d114f10eaefad92
Mrunal Yadav <yadavmrunal94@gmail.com>
2021-01-18 12:27:47 +0530

    Mrunal_Assignment_07

---
+++ src/mrunal/ConstantPath.java
@@ -0,0 +1,19 @@
+/*Assignment - 7 : Scroll & MultiSelect
+Step1 : Go to AutomationByKrishna.com
+Step2 : navigate to Basic Elements
+Step3 : select 1 and 3 value in a multi select dropdown
+Step 4 : Verify both values get selected
+Step 5 : print all options which not selected.
+Step 6 : deselect all selected options
+Step 7 : verify none of the options are selected now.*/
+package mrunal;
+
+public class ConstantPath {
+
+	public static final String chromeDriverPath 
+	= "./resources/windows/chromedriver.exe";
+	
+	public static final String chromeDriverKey 
+	= "webdriver.chrome.driver";
+
+}
