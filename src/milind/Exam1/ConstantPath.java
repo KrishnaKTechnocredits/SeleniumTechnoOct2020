81ec0132b1725207b0f1d855ac2a2c17bec1e08b
Milind Chavhan <ms.chavhan81@gmail.com>
2021-01-18 23:34:08 +0530

    MilindC_Exam1

---
+++ src/milind/Exam1/ConstantPath.java
@@ -0,0 +1,6 @@
+package milind.Exam1;
+
+public class ConstantPath {
+	public static final String chromeDriverPath = "./resources/windows/chromedriver.exe";
+	public static final String chromeDriverKey = "webdriver.chrome.driver";
+}
