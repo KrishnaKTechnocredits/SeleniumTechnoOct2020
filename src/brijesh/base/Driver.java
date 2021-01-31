2dd8d126370e451673229c9601c0a9c445d311c3
BrijeshGOct2020 <brijeshgajera4321@gmail.com>
2021-01-31 09:21:36 +0530

    BrijeshG_Assignment_16

--- src/brijesh/base/Driver.java
+++ src/brijesh/base/Driver.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
-	static WebDriver driver;
 	public static WebDriver start(String website) {
 		String os = System.getProperty("os.name").toLowerCase();
 		System.out.println("os : "+ os);
 		String path = os.contains("windows") ? "./resources/windows/chromedriver.exe"
 				: os.contains("mac") ? "./resources/mac/chromedriver" : null;
 		System.setProperty("webdriver.chrome.driver", path);
-		driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get(website);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
